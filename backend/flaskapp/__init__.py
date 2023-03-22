6119c4b244e9cb9e807964efbfb2068474ca19e1
Ata <ataserb@gmail.com>
2023-03-22 03:42:47 +0300

    add flask backend app, endpoints todo

---
+++ backend/flaskapp/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+
+
+from models import setup_db, Question, Category
+
+QUESTIONS_PER_PAGE = 10
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+
+
+    # TODO 
+
+    return app
